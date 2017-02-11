138f44f1bcc182f1b973d827b474c2fabbd5e6f2
nryn <narayanohanlon@gmail.com>
2017-02-11 20:26:58 +0000

    more tests, and everything's green.

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -16,4 +16,10 @@ describe 'fizzbuzz' do
   it 'returns "1" when passed 1' do
     expect(fizzbuzz(1)).to eq '1'
   end
+  it 'returns "17831" when passed 17831' do
+    expect(fizzbuzz(17831)).to eq '17831'
+  end
+  it 'returns "fizz" when passed 17832' do
+    expect(fizzbuzz(17832)).to eq 'fizz'
+  end
 end
